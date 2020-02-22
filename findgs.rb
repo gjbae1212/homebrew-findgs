4c7f0a8bc428813afd4835faab7fb722a970c0b1
gjbae1212 <gjbae1212@gmail.com>
2020-02-22 16:56:27 +0900

    Brew formula update for findgs version v1.0.5

--- findgs.rb
+++ findgs.rb
@@ -2,14 +2,14 @@
 class Findgs < Formula
   desc "findgs can search your starred repositories in the Github which matched searching text from title, description, topic, and README."
   homepage ""
-  version "1.0.3"
+  version "1.0.5"
 
   if OS.mac?
-    url "https://github.com/gjbae1212/findgs/releases/download/v1.0.3/findgs_1.0.3_Darwin_x86_64.tar.gz"
-    sha256 "5575c21f7b95ff37bb43a43e9e29b2d1b920fa32cbcd44933b3d172e6837472c"
+    url "https://github.com/gjbae1212/findgs/releases/download/v1.0.5/findgs_1.0.5_Darwin_x86_64.tar.gz"
+    sha256 "2a418a04828c60c558e9b1a29b755e2e80d6b23e86dc6c5b0691424f89cc6f2f"
   elsif OS.linux?
-    url "https://github.com/gjbae1212/findgs/releases/download/v1.0.3/findgs_1.0.3_Linux_x86_64.tar.gz"
-    sha256 "5a60696736cd78ad5e53d6e173aff0db61ead841439e8df9de8d16b9b4b50edf"
+    url "https://github.com/gjbae1212/findgs/releases/download/v1.0.5/findgs_1.0.5_Linux_x86_64.tar.gz"
+    sha256 "1ffc7ba0f4ead193016ed5a0dd216757ac2be61f6ecc0ea8a7e1721e7d7c81dd"
   end
 
   def install
