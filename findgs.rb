1bd6cab242a362b9dcb6c2da51dd0ef2efa1a37d
gjbae1212 <gjbae1212@gmail.com>
2020-08-13 12:53:58 +0900

    Brew formula update for findgs version v1.0.6

--- findgs.rb
+++ findgs.rb
@@ -2,14 +2,17 @@
 class Findgs < Formula
   desc "findgs can search your starred repositories in the Github which matched searching text from title, description, topic, and README."
   homepage ""
-  version "1.0.5"
+  version "1.0.6"
+  bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/gjbae1212/findgs/releases/download/v1.0.5/findgs_1.0.5_Darwin_x86_64.tar.gz"
-    sha256 "2a418a04828c60c558e9b1a29b755e2e80d6b23e86dc6c5b0691424f89cc6f2f"
+    url "https://github.com/gjbae1212/findgs/releases/download/v1.0.6/findgs_1.0.6_Darwin_x86_64.tar.gz"
+    sha256 "da9c70630a04b9d83c660f0e660f40b7635be6f5b8c9a2fc2a9214fde4cbd074"
   elsif OS.linux?
-    url "https://github.com/gjbae1212/findgs/releases/download/v1.0.5/findgs_1.0.5_Linux_x86_64.tar.gz"
-    sha256 "1ffc7ba0f4ead193016ed5a0dd216757ac2be61f6ecc0ea8a7e1721e7d7c81dd"
+    if Hardware::CPU.intel?
+      url "https://github.com/gjbae1212/findgs/releases/download/v1.0.6/findgs_1.0.6_Linux_x86_64.tar.gz"
+      sha256 "307e59bcffb942b892ce493a8002806f85f74c7e9c2ccb2b0fa383fd645a67f5"
+    end
   end
 
   def install
